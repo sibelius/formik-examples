d5f82342d17d9b83c526a6ce82066127cf673fba
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2019-07-21 23:04:04 -0300

    Updates

---
+++ precache-manifest.bb2ee14ee11beaf5f660c00c3531b679.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27342318dc16c8300afbed348a82037f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09bb211bfe36b537b2a5",
+    "url": "/static/js/2.b3c6c37b.chunk.js"
+  },
+  {
+    "revision": "a59b3de832298d3a241a",
+    "url": "/static/js/main.7fd1fe1f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
